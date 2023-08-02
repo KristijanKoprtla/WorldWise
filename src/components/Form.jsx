e7e07ebce4d5333f5dd7663430dda7415828a726
BombMarley <kile146@gmail.com>
2023-08-02 20:49:49 +0200

    If there is no lat and lng display Message

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -45,6 +45,8 @@ function Form() {
 
   useEffect(
     function () {
+      if (!lat && !lng) return;
+
       async function fetchCityData() {
         try {
           setIsLoadingGeocoding(true);
@@ -71,6 +73,9 @@ function Form() {
   );
 
   if (isLoadingGeocoding) return <Spinner />;
+
+  if (!lat && !lng)
+    return <Message message="Start by clicking somewhere on the map" />;
 
   if (geocodingError) return <Message message={geocodingError} />;
 
